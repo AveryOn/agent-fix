ea39c06d2dc9b500fea46df01c247999680e2cbe
AveryOn <lnkonion2@gmail.com>
2026-08-04 20:05:02 +0400

    [2026-08-04-1785859500] implement implementer agent index

---
+++ src/application/implementer/index.ts
@@ -0,0 +1,3 @@
+export * from '~/application/implementer/implementation-gate'
+export * from '~/application/implementer/implementation-patch-validator'
+export * from '~/application/implementer/implementer-agent'
